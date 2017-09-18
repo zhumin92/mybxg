198ea29cd5a8dda609355b3de7bbd550c80dd58c
zhumin92 <408898170@qq.com>
2017-09-18 18:31:10 +0800

    初始化1.0.0版本

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	header('content-type:text/html; charset=utf8');
+	//include
+	// $path = $_SERVER['PATH_INFO'];
+	// var_dump($_SERVER);
+	$dir = 'main';
+	$filename = 'index';
+
+	if(array_key_exists('PATH_INFO', $_SERVER)){
+		$path = $_SERVER['PATH_INFO'];
+		$str = substr($path, 1);
+		$ret = explode('/', $str);
+		if(count($ret) == 2){
+			$dir = $ret[0];
+			$filename = $ret[1];
+		}else{
+			$filename = 'login';
+		}
+	}
+
+
+	include('./views/'.$dir.'/'.$filename.'.html');
+	
+?>
