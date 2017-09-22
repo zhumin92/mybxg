31cc4c196964ae620ad859002e5697a8257bd986
zhumin92 <408898170@qq.com>
2017-09-22 10:05:12 +0800

    启用注销讲师

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -7,6 +7,30 @@ define(['jquery','template'],function($,template){
 			var html = template('teacherTpl',{list:data.result});
 			// console.log(html);
 			$('#teacherInfo').html(html);
+
+			//启用注销功能
+			$('.eod').click(function(){
+				var that = this;
+				var td = $(this).closest('td');
+				var tcId = td.attr('data-tcId');
+				var tcStatus = td.attr('data-status');
+				$.ajax({
+					type: 'post',
+					url: '/api/teacher/handle',
+					data: {tc_id: tcId, tc_status: tcStatus},
+					dataType: 'json',
+					success: function(data){
+						if(data.code == 200){
+							td.attr('data-status', data.result.tc_status);
+							if(data.result.tc_status ==0){
+								$(that).text('注销');
+							}else{
+								$(that).text('启用');
+							}
+						}
+					}
+				});
+			});
 		}
 	})
 })
